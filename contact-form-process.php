36d846a0bd44fbf7428b84c989970b587b107e6f
Cristina-ubb <mierla_cristina@yahoo.com>
2021-04-16 23:04:03 +0300

    Contact form

--- contact-form-process.php
+++ contact-form-process.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "crissu.pissu@gmail.com";
+    $email_to = "mierla.cris@gmail.com";
     $email_subject = "New form submission from Portfolio";
 
     function problem($error)
@@ -69,7 +69,7 @@ if (isset($_POST['Email'])) {
 
     <!-- include your success message below -->
 
-    Thank you for contacting us. We will be in touch with you very soon.
+    Thank you for contacting me. I will get in touch with you very soon.
 
 <?php
 }
